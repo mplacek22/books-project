143964e7f34f04058a97b592042a7200cf6ea776
Maja Placek <maja.placek451@gmail.com>
2024-05-31 01:39:04 +0200

    added pagination

--- database/src/main/java/com/mp/database/services/BookService.java
+++ database/src/main/java/com/mp/database/services/BookService.java
@@ -1,8 +1,6 @@
 package com.mp.database.services;
 
 import com.mp.database.domain.entities.BookEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,8 +10,6 @@ public interface BookService {
     BookEntity createUpdateBook(String isbn, BookEntity book);
 
     List<BookEntity> findAll();
-
-    Page<BookEntity> findAll(Pageable pageable);
 
     Optional<BookEntity> findOne(String isbn);
 
